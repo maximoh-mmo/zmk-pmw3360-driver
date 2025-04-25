ee90270240818da2292edf6706c02fe03700faaf
Max Heinze <maxheinze@googlemail.com>
2025-04-25 16:50:36 +0200

    oops

--- src/delay_basic.h
+++ src/delay_basic.h
@@ -34,8 +34,7 @@
 #ifndef _UTIL_DELAY_BASIC_H_
 #define _UTIL_DELAY_BASIC_H_ 1
 
-#include <
-.h>
+#include <inttypes.h>
 
 #if !defined(__DOXYGEN__)
 static __inline__ void _delay_loop_1(uint8_t __count) __attribute__((__always_inline__));
